775fb3a95dc61d5744910c7aafafa3d423d37e1d
dacharat <dacharat.p@ku.th>
2020-04-07 23:03:07 +0700

    :basecampy: add node cron

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import App from './app'
+import { updateRestaurantTrend } from './cron'
 
 const main = async () => {
   const app = new App(3000)
+  updateRestaurantTrend()
   await app.listen()
 }
 
